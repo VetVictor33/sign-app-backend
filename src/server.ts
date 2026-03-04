687c72945629a742d82fd4e162dc9c90ae223340
Victor Feliciano <115307935+VetVictor33@users.noreply.github.com>
2026-03-03 23:34:58 -0300

    feat: initialize server setup with Fastify and error handling

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import { registerRoutes } from "@/controllers/index.js";
+import { getEnv } from "@/env.js";
+import { registerErrorHandler } from "@/middleware/errorHandler.js";
+import cors from "@fastify/cors";
+import Fastify, { type FastifyInstance } from "fastify";
+
+const env = getEnv();
+
+const server: FastifyInstance = Fastify({
+  logger: {
+    level: "info",
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "SYS:standard",
+        ignore: "pid,hostname",
+        singleLine: false,
+      },
+    },
+  },
+  disableRequestLogging: true,
+});
+
+server.register(cors);
+registerErrorHandler(server);
+
+await registerRoutes(server);
+
+const start = async () => {
+  try {
+    const port = env.PORT;
+    await server.listen({ port, host: "0.0.0.0" });
+
+    server.log.info(`Server running on port ${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
